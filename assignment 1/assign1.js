d9dc65988fbf75ed7ae4cbc581ea11c12e382acf
Brian <bmessick12@gmail.com>
2021-06-08 20:51:15 -0600

    assignone

--- assignment 1/assign1.js
+++ assignment 1/assign1.js
@@ -4,3 +4,21 @@ if (person != null) {
   document.getElementById("demo").innerHTML =
   "Hello " + person + "! How are you today?";
 }
+function myFunction() {
+let a = parseFloat("10")
+let b = parseFloat("10.00")
+let c = parseFloat("10.33")
+let d = parseFloat("34 45 66")
+let e = parseFloat(" 60 ")
+let f = parseFloat("40 years")
+let g = parseFloat("He was 40")
+
+document.getElementById("demo").innerHTML =
+  a + "<br>" +
+  b + "<br>" +
+  c + "<br>" +
+  d + "<br>" +
+  e + "<br>" +
+  f + "<br>" +
+  g;
+}
