6e38afc65b6807a3916b0edd3fc7575f280b6193
Brian <bmessick12@gmail.com>
2021-06-09 18:58:16 -0600

    assingment1

---
+++ assignment 1/classassign.js
@@ -0,0 +1,25 @@
+var carName1 = "Volvo XC60";  // Double quotes
+var carName2 = 'Volvo XC60';  // Single quotes
+
+var x1 = true;
+var x2 = false;
+var x3 = new Date();
+var x4 = "999";
+var x5 = "999 888";
+
+var n =
+Number(x1) + "<br>" +
+Number(x2) + "<br>" +
+Number(x3) + "<br>" +
+Number(x4) + "<br>" +
+Number(x5);
+
+var car = {type:"Fiat", model:"500", color:"white"};
+
+var car1 = "Saab";
+var car2 = "Volvo";
+var car3 = "BMW";
+
+function myFunction(p1, p2) {
+    return p1 * p2;   // The function returns the product of p1 and p2
+  }
