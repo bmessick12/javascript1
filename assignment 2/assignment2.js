cdfbc1c64f66bde3b1a49d63b3ad8e6ede01f50f
Brian <bmessick12@gmail.com>
2021-06-16 17:38:28 -0600

    ;

--- assignment 2/assignment2.js
+++ assignment 2/assignment2.js
@@ -1,34 +1,67 @@
 
 
 function getMotto() {
-    return name;
+    let Name = prompt("What is your Motto?");
+    return Name;
 }
-
-let name = prompt("What is your Motto?");
-
-
-console.log (name);
-
 
 
 function getCount() {
+    let number = prompt("How many times would you like to see your Motto?")
+    return number;
 }
-let number = prompt("How many times would you like to see your Motto?")
+ 
 
-console.log (number);
-
-
-
-function printMotto() {
-    let number = 0;
+function printMotto(Name, number) {
     let printMotto = [];
     
-    for (let index = 0; index < number.length; index += 1) {
-      printMotto.push(name);
+    for (let index = 0; index < number; index += 1) {
+      printMotto.push(Name);
+      console.log(Name)
     }
     
     console.log(printMotto)
 }
-window.alert(name);
+// window.alert(name);
 
+let Name = getMotto(); 
+let number = getCount();
+printMotto(Name, number);
+
+
+// console.log 
+
+// const ProgressBar = require('progress')
+
+// const bar = new ProgressBar(':bar', { total: 10 })
+// const timer = setInterval(() => {
+//   bar.tick()
+//   if (bar.complete) {
+//     clearInterval(timer)
+//   }
+// }, 100)
+
+
+
+// function simpleInterest()
+
+// {
+// var pa,rate,time;
+// pa=parseFloat(prompt("Enter Principle Amount"));
+// rate=parseFloat(prompt("Enter Rate Amount"));
+// time=parseFloat(prompt("Enter Time(year)"));
+// document.write(pa + "</br>");
+// document.write(rate + "</br>");
+// document.write(time + "</br>");
+// var si=(pa*rate*time)/100;
+// document.write("Simple Interest anually is: "+si);
+// }
+// function area()
+// {
+// var radius;
+// radius=parseFloat(prompt("Enter radius"));
+// document.write(radius + "</br>");
+// var area = 3.14 * radius * radius;
+// document.write("Area of circle is is: "+area);
+// }
 
---
+++ assignment 4/assignment4.js
@@ -0,0 +1,57 @@
+
+'use strict';
+let age = prompt("how old are you?")
+let conf = confirm(`¿are you ${edad}?`)
+alert(`Confirmed, you´r age is ${conf}`)
+
+function simpleInterest()
+
+{
+var pa,rate,time;
+pa=parseFloat(prompt("Enter Principle Amount"));
+rate=parseFloat(prompt("Enter Rate Amount"));
+time=parseFloat(prompt("Enter Time(year)"));
+document.write(pa + "</br>");
+document.write(rate + "</br>");
+document.write(time + "</br>");
+var si=(pa*rate*time)/100;
+document.write("Simple Interest anually is: "+si);
+}
+function area()
+{
+var radius;
+radius=parseFloat(prompt("Enter radius"));
+document.write(radius + "</br>");
+var area = 3.14 * radius * radius;
+document.write("Area of circle is is: "+area);
+}
+
+
+
+
+function printMotto() {
+    let number = 0;
+    let printMotto = [];
+    
+    for (let index = 0; index < number.length; index += 1) {
+      let upperCaseName = number[index].toUpperCase();
+      printMotto.push(upperCaseName);
+    }
+    
+    console.log(printMotto)
+}
+window.alert(name);
+
+function getUrl() {
+  var url = String ('Enter image URL');
+  
+  
+  
+  
+  
+  function geturl() {
+    document.getElementById("geturl").innerHTML + window.location.href;
+    <input type="image" src="submit.gif" alt="Submit" style="float:right" width="48" height="48"></input>
+    document.body.appendChild(btn);
+  }
+  console.log(getUrl);
