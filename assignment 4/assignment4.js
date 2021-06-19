477d09855621db833359b757e1611089e300d1ce
Brian <bmessick12@gmail.com>
2021-06-19 08:12:04 -0600

    WHY?

--- assignment 4/assignment4.js
+++ assignment 4/assignment4.js
@@ -1,16 +1,110 @@
-function FV(PV,i,n){
-  var x=(1+i/100)
-  var FV=PV*(Math.pow(x,n))
-  return FV;
-  }
-  function Cal_FV(){
-  var pvalue=parseFloat(document.getElementById("a").value);
-  var interest=parseFloat(document.getElementById("b").value);
-  var num=parseInt(document.getElementById("c").value);
-  var fvalue=FV(pvalue, interest, num);
-  var fv=fvalue.toFixed(2);
-  document.getElementById('sub').textContent="The future value is= "+fv;
-  
-  }
+function checkNumber(input, min, max, msg)
+
+{
+
+    msg = msg + " field has invalid data: " + input.value;
 
 
+
+    var str = input.value;
+
+    for (var i = 0; i < str.length; i++) {
+
+        var ch = str.substring(i, i + 1)
+
+        if ((ch < "0" || "9" < ch) && ch != '.') {
+
+            alert(msg);
+
+            return false;
+           
+        }
+        
+    }
+
+    var num = 0 + str
+
+    if (num < min || max < num) {
+
+        alert(msg + " not in range [" + min + ".." + max + "]");
+
+        return false;
+
+    }
+    document.getElementById("value").innerHTML = "num";
+    input.value = str;
+
+    return true;
+
+}
+
+
+function computeField(input)
+
+{
+
+    if (input.value != null && input.value.length != 0)
+
+        input.value = "" + eval(input.value);
+
+    computeForm(input.form);
+
+}
+
+
+
+function computeForm(form)
+
+{
+
+    if ((form.payments.value == null || form.payments.value.length == 0) ||
+
+        (form.interest.value == null || form.interest.value.length == 0) ||
+
+        (form.principal.value == null || form.principal.value.length == 0)) {
+
+        return;
+
+    }
+
+
+
+    if (!checkNumber(form.payments, 1, 480, "# of payments") ||
+
+        !checkNumber(form.interest, .001, 99, "Interest") ||
+
+        !checkNumber(form.principal, 1, 10000000, "Principal")) {
+
+        form.fv.value = "Invalid";
+
+        return;
+
+    }
+
+    document.getElementById("rate").innerHTML = "i";
+
+    var i = form.interest.value;
+
+    if (i > 1.0) {
+
+        i = i / 100.0;
+
+        form.interest.value = i;
+
+    }
+
+    i /= 12;
+
+
+   var pow = form.principal.value;
+
+    for (var j = 0; j < form.payments.value *12; j++)
+
+        pow = (pow * i) + (pow *1);
+
+    form.fv.value = pow
+
+        form.totalint.value = (form.fv.value - form.principal.value)
+
+}
+document.getElementById("month").innerHTML = "pow";
