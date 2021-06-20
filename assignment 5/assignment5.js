99106d1b4f3b235e7b0c839fd29cf5f45b41e175
Brian <bmessick12@gmail.com>
2021-06-20 07:45:45 -0600

    WHY?

--- assignment 5/assignment5.js
+++ assignment 5/assignment5.js
@@ -1,72 +1,13 @@
+var up = document.getElementById('GFG_UP'); 
+        var down = document.getElementById('GFG_DOWN'); 
+          
+        function Fun() {
+            var img = document.createElement('img');
+            img.src = 'basic.gif';
+            document.getElementById('body').appendChild(img);
+        } 
 
-'use strict';
-let age = prompt("how old are you?")
-let conf = confirm(`¿are you ${edad}?`)
-alert(`Confirmed, you´r age is ${conf}`)
+        var today = new Date();
 
-function simpleInterest()
-
-{
-var pa,rate,time;
-pa=parseFloat(prompt("Enter Principle Amount"));
-rate=parseFloat(prompt("Enter Rate Amount"));
-time=parseFloat(prompt("Enter Time(year)"));
-document.write(pa + "</br>");
-document.write(rate + "</br>");
-document.write(time + "</br>");
-var si=(pa*rate*time)/100;
-document.write("Simple Interest anually is: "+si);
-}
-function area()
-{
-var radius;
-radius=parseFloat(prompt("Enter radius"));
-document.write(radius + "</br>");
-var area = 3.14 * radius * radius;
-document.write("Area of circle is is: "+area);
-}
-
-
-
-
-function printMotto() {
-    let number = 0;
-    let printMotto = [];
-    
-    for (let index = 0; index < number.length; index += 1) {
-      let upperCaseName = number[index].toUpperCase();
-      printMotto.push(upperCaseName);
-    }
-    
-    console.log(printMotto)
-}
-window.alert(name);
-
-function getUrl() {
-  var url = String ('Enter image URL');
-  
-  
-  
-  
-  
-  function geturl() {
-    document.getElementById("geturl").innerHTML + window.location.href;
-    <input type="image" src="submit.gif" alt="Submit" style="float:right" width="48" height="48"></input>
-    document.body.appendChild(btn);
-  }
-  console.log(getUrl);
-
-  imageForm.userImage.value;
-formName.inputName.value
-
-
-guide to ass 4
-
-onclick submit DataCue
-
-function submitData (){
-  if x is < 0 error 
-  call update dom 
-
-add check for each Number
-}
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        document.getElementById("p1").innerHTML = time;
